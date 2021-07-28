6afd587525b2b3c86765f5f655e889ca1eb9feb1
Andrey Maslikov <you@example.com>
2021-07-28 17:56:09 +0300

    create Entity User, and methods of service get,create,update

--- src/migrations/1627477139320-users.ts
+++ src/migrations/1627477139320-users.ts
@@ -3,21 +3,22 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class users1627477139320 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE public.users (
-          id integer NOT NULL,
+      `
+      CREATE SEQUENCE public.users_id_seq
+        START WITH 1
+        INCREMENT BY 1
+        NO MINVALUE
+        NO MAXVALUE
+        CACHE 1;
+      CREATE TABLE public.users (
+          id integer NOT NULL DEFAULT nextval('"users_id_seq"'::regclass),
           username character varying(25) NOT NULL,
           password character varying(64) NOT NULL,
           email character varying(254) NOT NULL,
           is_active boolean NOT NULL
-    );
-    CREATE SEQUENCE public.users_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-    ALTER TABLE ONLY public.users
-    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+      );
+      ALTER TABLE ONLY public.users
+        ADD CONSTRAINT users_pkey PRIMARY KEY (id);
     `,
     );
   }
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,19 @@
-export class User {}
+import { Entity, Column, PrimaryColumn } from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryColumn()
+  id: number;
+
+  @Column()
+  username: string;
+
+  @Column({ select: false })
+  password: string;
+
+  @Column()
+  email: string;
+
+  @Column({ default: true })
+  is_active: boolean;
+}
