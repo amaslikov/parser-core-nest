8799785f1bbe22a5ce672086fa65bea6a3aa83c3
Andrey Maslikov <you@example.com>
2021-07-28 16:50:26 +0300

    generate crud user, and orm connect

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as ormconfig from './config/ormconfig';
+
+export function DatabaseOrmModule(): DynamicModule {
+  // we could load the configuration from dotEnv here,
+  // but typeORM cli would not be able to find the configuration file.
+
+  return TypeOrmModule.forRoot(ormconfig);
+}
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(ormconfig), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
