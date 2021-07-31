8db75bf1959ba0766c47f33d4975dcd611f65101
Andrey Maslikov <you@example.com>
2021-07-31 22:45:22 +0300

    upload file music, now only for controller

--- src/modules/musics/musics.controller.ts
+++ src/modules/musics/musics.controller.ts
@@ -1,15 +1,45 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Request,
+  Delete,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { MusicsService } from './musics.service';
 import { CreateMusicDto } from './dto/create-music.dto';
 import { UpdateMusicDto } from './dto/update-music.dto';
+import * as fs from 'fs';
+import { FileInterceptor } from '@nestjs/platform-express';
+import mime = require('mime-types');
+import { v4 as uuidv4 } from 'uuid';
+import path = require('path');
 
 @Controller('musics')
 export class MusicsController {
   constructor(private readonly musicsService: MusicsService) {}
 
   @Post()
-  create(@Body() createMusicDto: CreateMusicDto) {
-    return this.musicsService.create(createMusicDto);
+  @UseInterceptors(FileInterceptor('file'))
+  async create(@Request() { user }, @UploadedFile() file) {
+    function ensureDirectoryExistence(filePath) {
+      const dirname = path.dirname(filePath);
+      if (fs.existsSync(dirname)) {
+        return true;
+      }
+      ensureDirectoryExistence(dirname);
+      fs.mkdirSync(dirname);
+    }
+    const ext = mime.extension(file.mimetype);
+    const fileName = `${uuidv4()}.${ext}`;
+    const filepath = `./upload/${user.id}/${fileName}`;
+    ensureDirectoryExistence(filepath);
+    await fs.promises.writeFile(filepath, file.buffer);
+    return { fileName };
   }
 
   @Get()
