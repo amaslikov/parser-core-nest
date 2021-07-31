a3135f96816277ea926029d168c2ece3673ea6d2
Andrey Maslikov <you@example.com>
2021-07-31 15:21:18 +0300

    fix VlidateMethod error

--- src/modules/common/validation/validate.decorator.ts
+++ src/modules/common/validation/validate.decorator.ts
@@ -1,5 +1,5 @@
 import { ObjectSchema } from 'joi';
-import { BadRequestException } from '@nestjs/common';
+import { BadRequestException, HttpStatus } from '@nestjs/common';
 
 export const ValidateMethod = (schema: ObjectSchema) => {
   return function (
@@ -12,7 +12,12 @@ export const ValidateMethod = (schema: ObjectSchema) => {
     propertyDesciptor.value = function (params: any) {
       const { error } = schema.validate(params, { abortEarly: false });
       if (error) {
-        const err = [...error.details, { propertyName }];
+        const err = {
+          statusCode: HttpStatus.BAD_REQUEST,
+          message: error.details,
+          propertyName,
+          error: 'Bad Request',
+        };
         throw new BadRequestException(err);
       }
       return method.apply(this, [params]);
