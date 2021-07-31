9d5dd33d7064f6442f203424d13cc78f1d15bfff
Andrey Maslikov <you@example.com>
2021-07-31 13:03:48 +0300

    all modulues move to an dir modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { ParseCarsModule } from './parse-cars/parse-cars.module';
-import { MusicsModule } from './musics/musics.module';
-import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { ParseCarsModule } from './modules/parse-cars/parse-cars.module';
+import { MusicsModule } from './modules/musics/musics.module';
+import { AuthModule } from './modules/auth/auth.module';
 import env = require('../env.js');
 import * as ormconfig from './config/ormconfig';
 
