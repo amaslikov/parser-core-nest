c19ee2624504c9c5efe5aec3062c300fcfbe1d1b
Andrey Maslikov <you@example.com>
2021-08-02 09:34:08 +0300

    for upload music, add record db and move all of this in a service layer

--- src/modules/musics/musics.controller.ts
+++ src/modules/musics/musics.controller.ts
@@ -13,11 +13,7 @@ import {
 import { MusicsService } from './musics.service';
 import { CreateMusicDto } from './dto/create-music.dto';
 import { UpdateMusicDto } from './dto/update-music.dto';
-import * as fs from 'fs';
 import { FileInterceptor } from '@nestjs/platform-express';
-import mime = require('mime-types');
-import { v4 as uuidv4 } from 'uuid';
-import path = require('path');
 
 @Controller('musics')
 export class MusicsController {
@@ -25,21 +21,12 @@ export class MusicsController {
 
   @Post()
   @UseInterceptors(FileInterceptor('file'))
-  async create(@Request() { user }, @UploadedFile() file) {
-    function ensureDirectoryExistence(filePath) {
-      const dirname = path.dirname(filePath);
-      if (fs.existsSync(dirname)) {
-        return true;
-      }
-      ensureDirectoryExistence(dirname);
-      fs.mkdirSync(dirname);
-    }
-    const ext = mime.extension(file.mimetype);
-    const fileName = `${uuidv4()}.${ext}`;
-    const filepath = `./upload/${user.id}/${fileName}`;
-    ensureDirectoryExistence(filepath);
-    await fs.promises.writeFile(filepath, file.buffer);
-    return { fileName };
+  async create(
+    @Request() { user },
+    @UploadedFile() file,
+    @Body('filename') filename,
+  ) {
+    return this.musicsService.create({ user, file, filename });
   }
 
   @Get()
--- src/modules/musics/musics.service.ts
+++ src/modules/musics/musics.service.ts
@@ -4,6 +4,12 @@ import { UpdateMusicDto } from './dto/update-music.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Music } from './entities/music.entity';
+import * as fs from 'fs';
+import * as path from 'path';
+import mime = require('mime-types');
+import { v4 as uuidv4 } from 'uuid';
+import { ValidateMethod } from '../common/validation/validate.decorator';
+import * as Joi from 'joi';
 
 @Injectable()
 export class MusicsService {
@@ -12,14 +18,45 @@ export class MusicsService {
     private musicRepository: Repository<Music>,
   ) {}
 
-  create(createMusicDto: CreateMusicDto) {
+  @ValidateMethod(
+    Joi.object({
+      user: Joi.object({
+        id: Joi.number().required(),
+      }).required(),
+      file: Joi.object({
+        buffer: Joi.binary().required(),
+        mimetype: Joi.string().required(),
+      }).required(),
+      filename: Joi.string().min(6).required(),
+    }).options({ allowUnknown: true }),
+  )
+  async create({ user, file, filename }) {
+    const filepath = await this.saveDisc(user, file);
     return this.musicRepository.save({
-      name: 'Dasha',
+      name: filename,
       position: 0,
-      path: 'somepath.mp3',
+      path: filepath,
       created_at: new Date().toISOString(),
       updated_at: new Date(),
     });
+  }
+
+  private async saveDisc(user, file) {
+    const ext = mime.extension(file.mimetype);
+    const filepath = `musics/${uuidv4()}.${ext}`;
+    const filepathRoot = `./upload/${user.id}/${filepath}`;
+    this.ensureDirectoryExistence(filepathRoot);
+    await fs.promises.writeFile(filepathRoot, file.buffer);
+    return filepath;
+  }
+
+  private ensureDirectoryExistence(filePath) {
+    const dirname = path.dirname(filePath);
+    if (fs.existsSync(dirname)) {
+      return true;
+    }
+    this.ensureDirectoryExistence(dirname);
+    fs.mkdirSync(dirname);
   }
 
   findAll() {
